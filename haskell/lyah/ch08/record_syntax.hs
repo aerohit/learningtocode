d01fcd6c630c86415c1fa74b3d751f99436e3483
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-26 16:40:19 +0200

    Record syntax.

---
+++ haskell/lyah/ch08/record_syntax.hs
@@ -0,0 +1,18 @@
+data Person = Person { firstName   :: String
+                     , lastName    :: String
+                     , age         :: Int
+                     , height      :: Float
+                     , phoneNumber :: String
+                     , flavor      :: String
+                     } deriving (Show)
+
+guy = Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
+
+data Car = Car { company         :: String
+               , model           :: String
+               , year            :: Int
+               } deriving (Show)
+
+ferrari = Car "Ferrari" "Ferrari" 1967
+-- order doesn't matter in record syntax
+mustang = Car {company="Ford", year=1967, model="Mustang"}
