4561f2b4a76d5a056df820a3092f90895ced7b23
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-26 08:34:08 +0200

    Intro to respond_to? and send

--- ruby/wellgrounded/ch02/ticket.rb
+++ ruby/wellgrounded/ch02/ticket.rb
@@ -26,3 +26,13 @@ end
 def ticket.available?
   false
 end
+
+print "Information desired: "
+request = gets.chomp
+
+# Alternatives to 'send' are '__send__' and 'public_send'
+if ticket.respond_to?(request)
+  puts ticket.send(request)
+else
+  puts "No such information available"
+end
