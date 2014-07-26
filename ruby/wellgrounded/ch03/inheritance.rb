66fc6767694609f79f0428bd3e064f2719e51a88
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-26 14:28:10 +0200

    Adding methods to objects.

--- ruby/wellgrounded/ch03/inheritance.rb
+++ ruby/wellgrounded/ch03/inheritance.rb
@@ -1,3 +1,14 @@
+class Person
+  def species
+    "Homo sapiens"
+  end
+end
+
+class Rubyist < Person
+end
+
+rubyist = Rubyist.new
+
 class Publication
   attr_accessor :publisher
 end
@@ -14,13 +25,11 @@ mag.publisher = "David A. Black"
 mag.editor = "Joe Smith"
 puts "Mag is published by #{mag.publisher}, and edited by #{mag.editor}."
 
-class Person
-  def species
-    "Homo sapiens"
-  end
+puts mag.is_a? Magazine
+puts mag.is_a? Publication
+
+def mag.wings
+  puts "Look! I can fly"
 end
 
-class Rubyist < Person
-end
-
-rubyist = Rubyist.new
+mag.wings
