d798ab20661f6818e9202940ab5191da60177a27
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-26 13:43:08 +0200

    Intro to inheritance.

---
+++ ruby/wellgrounded/ch03/inheritance.rb
@@ -0,0 +1,26 @@
+class Publication
+  attr_accessor :publisher
+end
+
+class Magazine < Publication
+  attr_accessor :editor
+end
+
+class Ezine < Magazine
+end
+
+mag = Magazine.new
+mag.publisher = "David A. Black"
+mag.editor = "Joe Smith"
+puts "Mag is published by #{mag.publisher}, and edited by #{mag.editor}."
+
+class Person
+  def species
+    "Homo sapiens"
+  end
+end
+
+class Rubyist < Person
+end
+
+rubyist = Rubyist.new
