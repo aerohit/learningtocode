e50f59b53b52a7c28c1341942d9173239a2df125
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-26 18:19:59 +0200

    Defined a recursive tree.

--- haskell/lyah/ch08/recursive_data_structures.hs
+++ haskell/lyah/ch08/recursive_data_structures.hs
@@ -5,3 +5,25 @@ infixr 5 .++
 (.++) :: List a -> List a -> List a
 Empty .++ ys      = ys
 (x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node v left right)
+    | x == v = Node v left right
+    | x < v  = Node v (treeInsert x left) right
+    | x > v  = Node v left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem _ EmptyTree = False
+treeElem x (Node v left right)
+    | x == v = True
+    | x < v  = treeElem x left
+    | x > v  = treeElem x right
+
+nums = [8,6,4,1,7,3,5]
+numsTree = foldr treeInsert EmptyTree nums
