1c1a42754fa7a4a19e326335dfdbd267e8af6cc5
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-26 09:57:00 +0200

    Default and optional arguments.

--- ruby/wellgrounded/ch02/object.rb
+++ ruby/wellgrounded/ch02/object.rb
@@ -14,6 +14,19 @@ def obj.product a, b
   puts "The product of #{a} and #{b} is #{a * b}"
 end
 
+def obj.multi_args(*x)
+  puts "I can take zero or more arguments #{x}"
+end
+
+def obj.args_unleased(a, b=1, *c, d, e)
+  puts "Arguments:"
+  p a, b, c, d, e
+end
+
 obj.talk
 obj.sum(3, 4)
 obj.product 3, 4
+
+obj.multi_args 1, 2, 3
+obj.args_unleased 1, 2, 3, 4, 5
+obj.args_unleased 9, 8, 7
