0c9d43c67243da8e66b18f54d4c6ac0c776d084b
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-26 15:43:32 +0200

    Reverse polish notation in haskell.

---
+++ haskell/rpn/reversepolishnotation.hs
@@ -0,0 +1,9 @@
+import Data.List
+-- Functionally Solving Problems: LYAH
+
+solveRPN :: (Num a, Read a) => String -> a
+solveRPN = head . foldl foldingFunction [] . words
+    where foldingFunction (x:y:ys) "*"    = (y * x):ys
+          foldingFunction (x:y:ys) "+"    = (y + x):ys
+          foldingFunction (x:y:ys) "-"    = (y - x):ys
+          foldingFunction xs numberString = read numberString:xs
