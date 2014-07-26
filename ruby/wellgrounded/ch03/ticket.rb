bd80dff37c4e652443b709ea9cfa2d7cf4962746
Rohit Saxena <aerohit.saxena@gmail.com>
2014-07-26 11:46:15 +0200

    Class initializers, getters and assignments methods.

---
+++ ruby/wellgrounded/ch03/ticket.rb
@@ -0,0 +1,30 @@
+class Ticket
+  def initialize(venue, date)
+    @venue = venue
+    @date  = date
+  end
+
+  def venue
+    @venue
+  end
+
+  def date
+    @date
+  end
+
+  def price=(amount)
+    @price = amount
+  end
+
+  def price
+    @price
+  end
+end
+
+th = Ticket.new("Town Hall", "11/12/13")
+th.price = 34
+cc = Ticket.new("Convention Center", "12/13/14")
+cc.price = 64
+puts "We've created two tickets."
+puts "The first is for a #{th.venue} event on #{th.date} and costs #{th.price}."
+puts "The second is for an event on #{cc.date} at #{cc.venue} and costs #{cc.price}."
